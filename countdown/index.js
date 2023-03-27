a96a223d5463760f2687e4b10ce80fe45c378966
Anuj Pokhrel <pokhrel.anuz7@gmail.com>
2023-03-26 22:41:32 -0400

    Update index.js

--- countdown/index.js
+++ countdown/index.js
@@ -64,5 +64,58 @@ function initializeClock(id, endtime) {
   var timeinterval = setInterval(updateClock, 1000);
 }
 
+function t_initializeClock(id, endtime) {
+  var clock = document.getElementById(id);
+  var daysSpan = clock.querySelector('.days');
+  var hoursSpan = clock.querySelector('.hours');
+  var minutesSpan = clock.querySelector('.minutes');
+  var secondsSpan = clock.querySelector('.seconds');
+  var daysshow = document.getElementById('t_daysshow');
+  var hoursshow = document.getElementById('t_hoursshow');
+  var minutesshow = document.getElementById('t_minutesshow');
+  var secondsshow = document.getElementById('t_secondsshow');
+
+  function updateClock() {
+    var t = getTimeRemaining(endtime);
+
+    daysSpan.innerHTML = t.days;
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
+      var c = document.getElementById('clockdiv');
+      c.style.display = "none";
+      var text = document.getElementById('message');
+      var title = document.getElementById('title');
+      text.innerHTML = "Nepal is where I be!!!"
+      title.style.display = "none";
+    }
+    if (t.days <= 0) {
+      daysshow.style.display = "none";
+      daysSpan.style.display = "none";
+    }
+    if (t.days <= 0 && t.hours <= 0) {
+      hoursshow.style.display = "none";
+      daysSpan.style.display = "none";
+    }
+    if (t.days <= 0 && t.hours <= 0 && t.minutes <= 0) {
+      minutesshow.style.display = "none";
+      daysSpan.style.display = "none";
+    }
+    if (t.days <= 0 && t.hours <= 0 && t.minutes <= 0 && t.seconds <= 0) {
+      secondsshow.style.dispaly = "none";
+      daysSpan.style.display = "none";
+    }
+
+  }
+
+  updateClock();
+  var timeinterval = setInterval(updateClock, 1000);
+}
+
 var deadline = new Date("Dec 14, 2023 12:00:00");
+var turkey_deadline = new Date("Jun 19, 2023 12:00:00");
 initializeClock('clockdiv', deadline);
+initializeClock('t_clockdiv', turkey_deadline);
